eca152461e8fd2d4c3e5cc9e257a0d4c4dda0ffc
Slava Cado <little.cado1@gmail.com>
2022-04-30 15:07:44 +0300

    Add webpack configs and image declarations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { FC } from "react"
+import './styles.css'
+import IMAGE from './logo.png'
 
 export const App: FC = () => {
-  return <h1> Aviasales </h1>
+  return <>
+  <h1> Aviasales {process.env.NODE_ENV} </h1>
+  <img src={IMAGE} alt="re" width="300" height="200" />
+  </>
 }
