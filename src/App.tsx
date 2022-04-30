1ac676bfd37f37166875cd8bbf05474feb311858
Slava Cado <little.cado1@gmail.com>
2022-04-30 15:40:08 +0300

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { FC } from "react"
+import { FC } from 'react'
 import './styles.css'
 import IMAGE from './logo.png'
 
 export const App: FC = () => {
-  return <>
-  <h1> Aviasales {process.env.NODE_ENV} </h1>
-  <img src={IMAGE} alt="re" width="300" height="200" />
-  </>
+  const f = 'fdfd'
+  return (
+    <>
+      <h1> Aviasales {process.env.NODE_ENV} </h1>
+      <img src={IMAGE} alt="re" width="300" height="200" />
+    </>
+  )
 }
