2e367e5ecc6db4353498c2326f43f979a37bc293
Slava Cado <little.cado1@gmail.com>
2022-06-03 19:37:47 +0300

    refactor: change arrow funcs to function declaration in helpers

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,4 +1,4 @@
-export const getHHMMfromDate = (date: Date) => {
+export function getHHMMfromDate(date: Date) {
   const hours = String(date.getHours())
   const hoursToPrint = hours.length === 2 ? hours : '0' + hours
 
@@ -8,7 +8,7 @@ export const getHHMMfromDate = (date: Date) => {
   return hoursToPrint + ':' + minutesToPrint
 }
 
-export const getTimeInterval = (time: string, minutes: number) => {
+export function getTimeInterval(time: string, minutes: number) {
   const d = new Date(Date.parse(time))
   const milliseconds = minutes * 60 * 1000
   const d2 = new Date(d.getTime() + milliseconds)
@@ -16,7 +16,7 @@ export const getTimeInterval = (time: string, minutes: number) => {
   return getHHMMfromDate(d) + ' - ' + getHHMMfromDate(d2)
 }
 
-export const getHHMMfromMinutes = (minutes: number) => {
+export function getHHMMfromMinutes(minutes: number) {
   const hours = Math.floor(minutes / 60)
   const mins = Math.floor(minutes - hours * 60)
 
