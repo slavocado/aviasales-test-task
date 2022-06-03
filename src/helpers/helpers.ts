c01d80796b01300dc7f61887728649e83cc84745
Slava Cado <little.cado1@gmail.com>
2022-06-03 20:44:26 +0300

    refactor: change "magic numbers" to const, add comments to clarify

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,8 +1,14 @@
+const MINUTES_IN_HOUR = 60
+const SECONDS_IN_MINUTE = 60
+const MILLISECONS_IN_SECOND = 1000
+
 export function getHHMMfromDate(date: Date) {
   const hours = String(date.getHours())
+  // if hours has one digit - add 0 to its begining
   const hoursToPrint = hours.length === 2 ? hours : '0' + hours
 
   const minutes = String(date.getMinutes())
+  // if minutes has one digit - add 0 to its begining
   const minutesToPrint = minutes.length === 2 ? minutes : '0' + minutes
 
   return `${hoursToPrint} : ${minutesToPrint}`
@@ -10,15 +16,15 @@ export function getHHMMfromDate(date: Date) {
 
 export function getTimeInterval(time: string, minutes: number) {
   const firstDate = new Date(Date.parse(time))
-  const milliseconds = minutes * 60 * 1000
+  const milliseconds = minutes * SECONDS_IN_MINUTE * MILLISECONS_IN_SECOND
   const secondDate = new Date(firstDate.getTime() + milliseconds)
 
   return getHHMMfromDate(firstDate) + ' - ' + getHHMMfromDate(secondDate)
 }
 
 export function getHHMMfromMinutes(minutes: number) {
-  const hours = Math.floor(minutes / 60)
-  const mins = Math.floor(minutes - hours * 60)
+  const hours = Math.floor(minutes / MINUTES_IN_HOUR)
+  const mins = Math.floor(minutes - hours * MINUTES_IN_HOUR)
 
   return `${hours}ч ${mins}м`
 }
